a7d7712a2b43785a7201a7a21461eebc1b29f544
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2022-10-11 09:57:15 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    control.reset()
 })
+radio.onReceivedValue(function (name, value) {
+    serial.writeValue(name, (value * 9.81 / 1024 - 9.81) * -1)
+    serial.writeLine("" + (input.runningTime() / 1000))
+    led.toggle(0, 0)
+})
+radio.setGroup(1)
