576a1238e9c10a46a289f9b25476cf17a810055e
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2022-10-14 10:51:06 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,28 @@ input.onButtonPressed(Button.A, function () {
     control.reset()
 })
 radio.onReceivedValue(function (name, value) {
-    serial.writeValue(name, (value * 9.81 / 1024 - 9.81) * -1)
+    if (name == "x") {
+        acc_x = value
+    }
+    if (name == "y") {
+        acc_y = value
+    }
+    if (name == "z") {
+        acc_z = value
+    }
+})
+let acc_z = 0
+let acc_y = 0
+let acc_x = 0
+acc_x = 0
+acc_y = 0
+acc_z = 0
+radio.setGroup(1)
+basic.pause(1000)
+radio.sendString("go")
+basic.forever(function () {
+    serial.writeValue("acc", Math.sqrt(acc_x * acc_x + (acc_y * acc_y + acc_z * acc_z)) * 9.81 / 1024 - 9.81)
     serial.writeLine("" + (input.runningTime() / 1000))
     led.toggle(0, 0)
+    basic.pause(100)
 })
-radio.setGroup(1)
